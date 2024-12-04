d915e00d317e0f416f295be10e84998f52fdae17
Sztruharis <sandra.sztruharova@gmail.com>
2024-12-04 09:25:17 +0100

    private/public v2

--- src/app/auth/odhlasenie/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/auth/odhlasenie/page.tsx
-
-import SignOutView from "@/sections/SignOutView";
-
-export const metadata = { title: "Odhlásenie | ZoškaSnap" };
-
-export default function SignOutPage() {
-  return <SignOutView />;
-}
--- src/app/auth/registracia/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/auth/registracia/page.tsx
-
-import SignUpView from "@/sections/SignUpView";
-
-export const metadata = { title: "Registrácia užívateľa | ZoškaSnap" };
-
-export default function SignUpPage() {
-  return <SignUpView />;
-}
